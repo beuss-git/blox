3d08790c465476a83064778c94fa2dea3775f2b6
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-05-15 21:43:54 +0200

    Main: Add test code for negating

--- src/blang/main.rs
+++ src/blang/main.rs
@@ -1,14 +1,15 @@
 use super::chunk::*;
 use super::opcode;
+use super::vm::VM;
 
 pub(crate) fn main() {
     let mut chunk = Chunk::new();
     chunk.add_constant(3.5, 0);
+    chunk.add_instruction(opcode::OP_NEGATE, 0);
     chunk.add_instruction(opcode::OP_RETURN, 0);
-    chunk.add_instruction(opcode::OP_RETURN, 0);
-    chunk.add_instruction(opcode::OP_RETURN, 1);
-    chunk.add_instruction(opcode::OP_RETURN, 2);
-    chunk.add_instruction(opcode::OP_RETURN, 2);
 
-    chunk.disassemble_chunk("test chunk");
+    //chunk.disassemble_chunk("test chunk");
+
+    let mut vm = VM::new(chunk);
+    vm.interpret();
 }
