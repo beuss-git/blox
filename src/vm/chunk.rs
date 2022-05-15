f3bcc232984b856e80b8ace9d6bb5df6837d2c3a
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-05-15 20:13:36 +0200

    VM: Add chunk line data, compressed using RLE

--- src/vm/chunk.rs
+++ src/vm/chunk.rs
@@ -4,26 +4,30 @@ use super::value::*;
 pub struct Chunk {
     code: Vec<u8>,
     constants: ValueArray,
+    line_data: Vec<u8>, // Lets assume that a line can't have more than 255 bytes
 }
 
 /// Prints the instruction and returns the offset to the next instruction.
-fn simple_instruction(name: &str, offset: usize) -> usize {
-    println!("{}", name);
+fn simple_instruction(name: &str, chunk: &Chunk, offset: usize) -> usize {
+    println!("{}: {}", chunk.get_line(offset), name);
     offset + 1
 }
 
+/// Prints the instruction and returns the offset to the next instruction.
 fn constant_instruction(name: &str, chunk: &Chunk, offset: usize) -> usize {
     let constant_index = chunk.read_chunk(offset + 1);
-    print!("{} {}, ", name, constant_index);
+    print!("{}: {} {}, ", chunk.get_line(offset), name, constant_index);
     chunk.print_value(constant_index as usize);
     offset + 2
 }
 
 impl Chunk {
     pub fn new() -> Self {
+        // TODO: Preallocate the code and line data arrays (?)
         Self {
             code: Vec::new(),
             constants: ValueArray::new(),
+            line_data: Vec::new(),
         }
     }
 
@@ -31,8 +35,28 @@ impl Chunk {
         self.code[offset]
     }
     /// Adds byte to the chunk
-    fn write_chunk(&mut self, byte: u8) {
+    fn write_chunk(&mut self, byte: u8, line: usize) {
+        // RLE compression of line data
+        if self.line_data.len() == line + 1 {
+            self.line_data[line] += 1;
+        } else {
+            self.line_data.push(1);
+        }
         self.code.push(byte);
+    }
+
+    // https://www.csfieldguide.org.nz/en/chapters/coding-compression/run-length-encoding/
+    fn get_line(&self, offset: usize) -> usize {
+        let mut total: usize = 0;
+        for (line, length) in self.line_data.iter().enumerate() {
+            total += *length as usize;
+            if total > offset {
+                return line;
+            }
+        }
+
+        assert!(false);
+        return 0;
     }
 
     pub fn get_value(&self, index: usize) -> Value {
@@ -44,18 +68,18 @@ impl Chunk {
     }
 
     /// Adds instruction into our chunk
-    pub fn add_instruction(&mut self, instruction: u8) {
-        self.write_chunk(instruction);
+    pub fn add_instruction(&mut self, instruction: u8, line: usize) {
+        self.write_chunk(instruction, line);
     }
 
     /// Adds constant into our chunk and returns the index of the constant
-    pub fn add_constant(&mut self, value: Value) -> usize {
-        self.write_chunk(opcode::OP_CONSTANT);
+    pub fn add_constant(&mut self, value: Value, line: usize) -> usize {
+        self.write_chunk(opcode::OP_CONSTANT, line);
         self.constants.add_value(value);
         let index = self.constants.len() - 1;
 
         // NOTE: Currently limited to 255 constants
-        self.write_chunk(index as u8);
+        self.write_chunk(index as u8, line);
 
         index
     }
@@ -80,7 +104,7 @@ impl Chunk {
         let name = opcode::get_name(*instruction);
 
         match *instruction {
-            opcode::OP_RETURN => simple_instruction(name, offset),
+            opcode::OP_RETURN => simple_instruction(name, &self, offset),
             opcode::OP_CONSTANT => constant_instruction(name, &self, offset),
             _ => {
                 println!("Invalid opcode {}", self.code[offset] as u8);
--- src/vm/main.rs
+++ src/vm/main.rs
@@ -3,8 +3,12 @@ use super::opcode;
 
 pub(crate) fn main() {
     let mut chunk = Chunk::new();
-    chunk.add_constant(3.0);
-    chunk.add_instruction(opcode::OP_RETURN);
+    chunk.add_constant(3.5, 0);
+    chunk.add_instruction(opcode::OP_RETURN, 0);
+    chunk.add_instruction(opcode::OP_RETURN, 0);
+    chunk.add_instruction(opcode::OP_RETURN, 1);
+    chunk.add_instruction(opcode::OP_RETURN, 2);
+    chunk.add_instruction(opcode::OP_RETURN, 2);
 
     chunk.disassemble_chunk("test chunk");
 }
