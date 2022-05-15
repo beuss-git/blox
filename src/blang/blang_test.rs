2ed158e229a924b576097075f89998384142493c
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-05-15 23:31:58 +0200

    Add example tests to test CI

---
+++ src/blang/blang_test.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_fails() {
+        assert!(false);
+    }
+
+    #[test]
+    fn it_works() {
+        assert!(true);
+    }
+}
--- src/blang/mod.rs
+++ src/blang/mod.rs
@@ -1,3 +1,4 @@
+pub mod blang_test;
 pub mod chunk;
 pub mod main;
 pub mod opcode;
