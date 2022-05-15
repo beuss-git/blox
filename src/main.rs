e5d1e5270f02ec81a0534896adc3880e3b8b3a7d
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-05-15 16:33:03 +0200

    Main: Add simple repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    // REPL
+    loop {
+        print!("> ");
+        std::io::stdout()
+            .flush()
+            .expect("Failed to flush standard output");
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+        if input.trim() == "exit" {
+            break;
+        }
+
+        input.clear();
+    }
 }
