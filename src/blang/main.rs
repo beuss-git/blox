353c9b1000f5ba49a794ebe49ffc8d7f7d383fef
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-05-15 21:49:15 +0200

    Main: Add small test for binary multiplication

--- src/blang/main.rs
+++ src/blang/main.rs
@@ -4,8 +4,15 @@ use super::vm::VM;
 
 pub(crate) fn main() {
     let mut chunk = Chunk::new();
+    /*
     chunk.add_constant(3.5, 0);
     chunk.add_instruction(opcode::OP_NEGATE, 0);
+    chunk.add_instruction(opcode::OP_RETURN, 0);
+    */
+
+    chunk.add_constant(3.0, 0);
+    chunk.add_constant(2.0, 0);
+    chunk.add_instruction(opcode::OP_MULTIPLY, 0);
     chunk.add_instruction(opcode::OP_RETURN, 0);
 
     //chunk.disassemble_chunk("test chunk");
