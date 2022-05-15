f6098ec522ab25a5c98a3cf6f66f3e9232944ac5
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-05-15 18:01:17 +0200

    Main: Add small test code for creating and disassembling chunk

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use std::io::Write;
 
+mod vm;
+use crate::vm::chunk::*;
+use crate::vm::opcodes::*;
+
 fn main() {
-    let mut input = String::new();
+    let mut chunk = Chunk { code: Vec::new() };
+    chunk.code.push(OpCode::Return);
+
+    chunk.disassemble_chunk("test chunk");
+
+    println!("Capacity: {}", chunk.code.capacity());
+    println!("Len: {}", chunk.code.len());
+    println!("Data: {:?}", chunk.code);
 
     // REPL
+    /*
+    let mut input = String::new();
     loop {
         print!("> ");
         std::io::stdout()
@@ -18,4 +31,5 @@ fn main() {
 
         input.clear();
     }
+    */
 }
