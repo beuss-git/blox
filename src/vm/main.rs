4868de23e549c015564eca0a070e449243eb2834
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-05-15 19:54:31 +0200

    VM: Simplify main

--- src/vm/main.rs
+++ src/vm/main.rs
@@ -3,11 +3,8 @@ use super::opcode;
 
 pub(crate) fn main() {
     let mut chunk = Chunk::new();
-    chunk.add_instruction(opcode::OP_CONSTANT);
     chunk.add_constant(3.0);
     chunk.add_instruction(opcode::OP_RETURN);
 
     chunk.disassemble_chunk("test chunk");
-    println!("{}", opcode::get_name(opcode::OP_CONSTANT));
-    println!("{}", opcode::get_name(10));
 }
