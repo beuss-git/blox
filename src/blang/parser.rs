258c4fda1d88ce52a061f5a9600fc13c8a1ee24a
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-05-16 18:16:44 +0200

    Add Parser

---
+++ src/blang/parser.rs
@@ -0,0 +1,21 @@
+use super::lexer::{Token, TokenKind};
+pub struct Parser {
+    pub current: Token,
+    pub previous: Token,
+    pub had_error: bool,
+    pub panic_mode: bool,
+}
+
+impl Parser {
+    pub fn new() -> Self {
+        Self {
+            current: Token::new(TokenKind::Eof),
+            previous: Token::new(TokenKind::Eof),
+            had_error: false,
+            panic_mode: false,
+        }
+    }
+    pub fn is_at_end(&self) -> bool {
+        self.current.kind == TokenKind::Eof
+    }
+}
